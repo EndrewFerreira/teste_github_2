cc068390c893dfe4be547c2369e91c4d8bf18c4d
EndrewFerreira <ferreira10980@gmail.com>
2025-02-17 17:20:21 -0300

    terceira mudança no exe001.py
    desc

--- exe001.py
+++ exe001.py
@@ -1,3 +1,10 @@
 n = int(input('digite um número:'))
 print(f'o numero que você digitou foi {n}')
 print('='*50)
+if n < 5:
+  print('o numero digitado é menor que 5')
+elif n == 5:
+  print('o numero digitado é 5')
+elif n > 5:
+  print('o numero digitado é maior que 5')
+  
